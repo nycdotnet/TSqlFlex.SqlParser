6f02c19a7ff870e3c359a986f7111b491bba91e0
Steve Ognibene <steve.ognibene@outlook.com>
2014-06-21 08:45:26 -0400

    Initial commit.

---
+++ TSqlFlex.SqlParser.Tests/ParserTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TSqlFlex.SqlParser;
+
+namespace TSqlFlex.SqlParser.Tests
+{
+    [TestFixture()]
+    public class ParserTests
+    {
+
+        [Test()]
+        public void CreateParser_DoesNotCrash()
+        {
+            SqlBatch b = new SqlBatch("");
+            Assert.IsNotNull(b, "should be able to create batch.");
+        }
+
+        [Test()]
+        public void SqlBatch_ReturnsSameBatchTextAsPassed()
+        {
+            string s = Load_SimpleSelect();
+            SqlBatch b = new SqlBatch(s);
+            
+            Assert.AreEqual(s, b.BatchText());
+            Assert.AreEqual(21, b.BatchText().Length, "expected this specific length because that's what is in the file.");
+        }
+
+
+
+        private string Load_SimpleSelect()
+        {
+            string resourceName = "TSqlFlex.SqlParser.Tests.SqlScripts.SimpleSelect.sql";
+            string result;
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
---
+++ TSqlFlex.SqlParser/SqlBatch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSqlFlex.SqlParser
+{
+    public class SqlBatch
+    {
+        private List<object> Tokens = new List<object>();
+        private List<int> LineStartIndexes = new List<int>();
+        private char[] BatchChars;
+
+        private enum ParseStates
+        {
+            InAComment,
+            InAString,
+            Normal
+        }
+
+        private void parseSqlBatch()
+        {
+            int charIndex = 0;
+            var parseState = ParseStates.Normal;
+            while (charIndex < BatchChars.Length)
+            {
+                if (parseState == ParseStates.Normal)
+                {
+                    if (BatchChars[charIndex] == '-' && BatchChars[charIndex + 1] == '-')
+                    {
+
+                    }
+                }
+                charIndex += 1;
+            }
+        }
+
+        public SqlBatch(string sqlBatchText)
+        {
+            BatchChars = sqlBatchText.ToCharArray();
+            parseSqlBatch();
+        }
+
+        public SqlBatch(char[] sqlBatchChars)
+        {
+            BatchChars = sqlBatchChars;
+            parseSqlBatch();
+        }
+
+        public string BatchText()
+        {
+            return new string(BatchChars);
+        }
+
+
+    }
+}
