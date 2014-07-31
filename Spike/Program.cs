65f7caee9eb650d32026f61a16bac61dc741122e
Steve Ognibene <steve.ognibene@outlook.com>
2014-07-30 21:48:50 -0400

    clearing out test code from demo program

--- Spike/Program.cs
+++ Spike/Program.cs
@@ -16,28 +16,8 @@ namespace Spike
 
         static async void DoStuff()
         {
-            var actualTask = SqlTokenizer.TokenizeAsync("   --This is a comment\n ");
-            var expected = new List<SqlToken>();
-            expected.Add(new SqlToken(SqlToken.TokenTypes.Whitespace, 1, 1));
-            expected[0].Text = "   ";
-            expected.Add(new SqlToken(SqlToken.TokenTypes.LineCommentStart, 1, 4));
-            expected[1].Text = "--";
-            expected.Add(new SqlToken(SqlToken.TokenTypes.LineCommentBody, 1, 6));
-            expected[2].Text = "This is a comment";
-            expected.Add(new SqlToken(SqlToken.TokenTypes.Newline, 1, 23));
-            expected[3].Text = "\r\n";
-            expected.Add(new SqlToken(SqlToken.TokenTypes.Whitespace, 2, 1));
-            expected[4].Text = " ";
+            var actualTask = SqlTokenizer.TokenizeAsync("");
             var actual = await actualTask;
-            //Assert.AreEqual(expected, actual);
-            Console.Write(actual[0].StartCharacterIndex);
-            //Assert.AreEqual(expected, actual);
-            /*Assert.AreEqual(expected[0].TokenType, actual[0].TokenType);
-            Assert.AreEqual(expected[0].LineNumber, actual[0].LineNumber);
-            Assert.AreEqual(expected[0].StartCharacterIndex, actual[0].StartCharacterIndex);
-            Assert.AreEqual(expected[0].Length, actual[0].Length);
-            Assert.AreEqual(expected[0].Text, actual[0].Text);
-             * */
         }
     }
 }
